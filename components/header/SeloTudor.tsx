f1a6a3e386585a68e08dff6a6051b4b427abc1ef
regiscuba2b <reginaldo.cuba@totvs.com.br>
2024-10-25 10:17:13 -0300

    fix: logo tudor e rolex

--- components/header/SeloTudor.tsx
+++ components/header/SeloTudor.tsx
@@ -8,19 +8,18 @@ export default function SeloTudor() {
 
   const isTudor = displaySeloTudor.value;
 
+  const showLogoTudor = (isTudor || url);
+
   return (
     <>
-      {(isTudor || url) &&
-        (
-          <a class="mr-2" href="/tudor">
-            <div class="relative z-[-1]">
-              <img
-                class="w-[120px]"
-                src="https://www.dryzun.com.br/arquivos/Tudor-plaque-240x120_en.jpg?v=638282558950030000"
-              />
-            </div>
-          </a>
-        )}
+      <a class={`noooaa-23 mr-2 ${showLogoTudor ? 'db' : 'hidden'}`} href="/tudor">
+        <div class="relative z-[-1]">
+          <img
+            class="w-[120px]"
+            src="https://www.dryzun.com.br/arquivos/Tudor-plaque-240x120_en.jpg?v=638282558950030000"
+          />
+        </div>
+      </a>
     </>
   );
 }
