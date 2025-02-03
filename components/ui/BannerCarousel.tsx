1fe85860befc38c83b220ce9f3e71829d263f8e9
ERIC MACEDO GOMES <eric.gomes@totvs.com.br>
2025-02-03 10:59:53 -0300

    fix: banner home

--- components/ui/BannerCarousel.tsx
+++ components/ui/BannerCarousel.tsx
@@ -1,7 +1,7 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import {
-  SendEventOnClick,
-  SendEventOnView,
+    SendEventOnClick,
+    SendEventOnView,
 } from "../../components/Analytics.tsx";
 import Button from "../../components/ui/Button.tsx";
 import Icon from "../../components/ui/Icon.tsx";
@@ -12,241 +12,244 @@ import { useId } from "../../sdk/useId.ts";
  * @titleBy alt
  */
 export interface Banner {
-  /** @description desktop otimized image */
-  desktop: ImageWidget;
-  /** @description mobile otimized image */
-  mobile: ImageWidget;
-  /** @description Image's alt text */
-  alt: string;
-  action?: {
-    /** @description when user clicks on the image, go to this link */
-    href?: string;
-    /** @description Image text title */
-    title?: string;
-    /** @description Image text subtitle */
-    subTitle?: string;
-    /** @description Button label */
-    label?: string;
-  };
+    /** @description desktop otimized image */
+    desktop: ImageWidget;
+    /** @description mobile otimized image */
+    mobile: ImageWidget;
+    /** @description Image's alt text */
+    alt: string;
+    action?: {
+        /** @description when user clicks on the image, go to this link */
+        href?: string;
+        /** @description Image text title */
+        title?: string;
+        /** @description Image text subtitle */
+        subTitle?: string;
+        /** @description Button label */
+        label?: string;
+    };
 }
 
 export interface Props {
-  images?: Banner[];
-  /**
-   * @description Check this option when this banner is the biggest image on the screen for image optimizations
-   */
-  preload?: boolean;
-  /**
-   * @title Show arrows
-   * @description show arrows to navigate through the images
-   */
-  arrows?: boolean;
-  /**
-   * @title Show dots
-   * @description show dots to navigate through the images
-   */
-  dots?: boolean;
-  /**
-   * @title Autoplay interval
-   * @description time (in seconds) to start the carousel autoplay
-   */
-  interval?: number;
+    images?: Banner[];
+    /**
+     * @description Check this option when this banner is the biggest image on the screen for image optimizations
+     */
+    preload?: boolean;
+    /**
+     * @title Show arrows
+     * @description show arrows to navigate through the images
+     */
+    arrows?: boolean;
+    /**
+     * @title Show dots
+     * @description show dots to navigate through the images
+     */
+    dots?: boolean;
+    /**
+     * @title Autoplay interval
+     * @description time (in seconds) to start the carousel autoplay
+     */
+    interval?: number;
 }
 
 const DEFAULT_PROPS = {
-  images: [
-    {
-      alt: "/feminino",
-      action: {
-        title: "New collection",
-        subTitle: "Main title",
-        label: "Explore collection",
-        href: "/",
-      },
-      mobile:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/c007e481-b1c6-4122-9761-5c3e554512c1",
-      desktop:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
-    },
-    {
-      alt: "/feminino",
-      action: {
-        title: "New collection",
-        subTitle: "Main title",
-        label: "Explore collection",
-        href: "/",
-      },
-      mobile:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/c007e481-b1c6-4122-9761-5c3e554512c1",
-      desktop:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
-    },
-    {
-      alt: "/feminino",
-      action: {
-        title: "New collection",
-        subTitle: "Main title",
-        label: "Explore collection",
-        href: "/",
-      },
-      mobile:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/c007e481-b1c6-4122-9761-5c3e554512c1",
-      desktop:
-        "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
-    },
-  ],
-  preload: true,
+    images: [
+        {
+            alt: "/feminino",
+            action: {
+                title: "New collection",
+                subTitle: "Main title",
+                label: "Explore collection",
+                href: "/",
+            },
+            mobile:
+                "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/c007e481-b1c6-4122-9761-5c3e554512c1",
+            desktop:
+                "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
+        },
+        {
+            alt: "/feminino",
+            action: {
+                title: "New collection",
+                subTitle: "Main title",
+                label: "Explore collection",
+                href: "/",
+            },
+            mobile:
+                "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/c007e481-b1c6-4122-9761-5c3e554512c1",
+            desktop:
+                "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
+        },
+        {
+            alt: "/feminino",
+            action: {
+                title: "New collection",
+                subTitle: "Main title",
+                label: "Explore collection",
+                href: "/",
+            },
+            mobile:
+                "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/c007e481-b1c6-4122-9761-5c3e554512c1",
+            desktop:
+                "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/2291/d057fc10-5616-4f12-8d4c-201bb47a81f5",
+        },
+    ],
+    preload: true,
 };
 
 function BannerItem(
-  { image, lcp, id }: { image: Banner; lcp?: boolean; id: string },
+    { image, lcp, id }: { image: Banner; lcp?: boolean; id: string },
 ) {
-  const {
-    alt,
-    mobile,
-    desktop,
-    action,
-  } = image;
+    const {
+        alt,
+        mobile,
+        desktop,
+        action,
+    } = image;
 
-  return (
-    <a
-      id={id}
-      href={action?.href ?? "#"}
-      aria-label={action?.label}
-      class="relative overflow-y-hidden w-full"
-    >
-      {action && (
-        <div class="absolute top-0 md:bottom-0 bottom-1/2 left-0 right-0 sm:right-auto max-w-[407px] flex flex-col justify-end gap-4 px-8 py-12">
-          <span class="text-2xl font-light text-base-100">
-            {action.title}
-          </span>
-          <span class="font-normal text-4xl text-base-100">
-            {action.subTitle}
-          </span>
-          {action.label && (
-            <Button
-              class="bg-base-100 text-sm font-light py-4 px-6 w-fit"
-              aria-label={action.label}
-            >
-              {action.label}
-            </Button>
-          )}
-        </div>
-      )}
-      <img
-        class="w-full h-full hidden md:block"
-        loading={lcp ? "eager" : "lazy"}
-        src={desktop}
-        alt={alt}
-      />
-      <img
-        class="md:hidden"
-        loading={lcp ? "eager" : "lazy"}
-        src={mobile}
-        alt={alt}
-      />
-    </a>
-  );
+    return (
+        <a
+            id={id}
+            href={action?.href ?? "#"}
+            aria-label={action?.label}
+            class="relative overflow-y-hidden w-full"
+        >
+            {action && (
+                <div class="absolute top-0 md:bottom-0 bottom-1/2 left-0 right-0 sm:right-auto max-w-[407px] flex flex-col justify-end gap-4 px-8 py-12">
+                    <span class="text-2xl font-light text-base-100">
+                        {action.title}
+                    </span>
+                    <span class="font-normal text-4xl text-base-100">
+                        {action.subTitle}
+                    </span>
+                    {action.label && (
+                        <Button
+                            class="bg-base-100 text-sm font-light py-4 px-6 w-fit"
+                            aria-label={action.label}
+                        >
+                            {action.label}
+                        </Button>
+                    )}
+                </div>
+            )}
+            <div class="overflow-hidden">
+                <img
+                    class="hidden md:block w-full aspect-[24/10] object-cover"
+                    loading={lcp ? "eager" : "lazy"}
+                    src={desktop}
+                    alt={alt}
+                />
+
+                <img
+                    class="md:hidden w-full aspect-[9/16] object-cover"
+                    loading={lcp ? "eager" : "lazy"}
+                    src={mobile}
+                    alt={alt}
+                />
+            </div>
+        </a>
+    );
 }
 
 function Dots({ images, interval = 0 }: Props) {
-  return (
-    <>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `
+    return (
+        <>
+            <style
+                dangerouslySetInnerHTML={{
+                    __html: `
           @property --dot-progress {
             syntax: '<percentage>';
             inherits: false;
             initial-value: 0%;
           }
           `,
-        }}
-      />
-      <ul class="carousel justify-center col-span-full gap-6 z-8 row-start-4 max-md:absolute max-md:w-full max-md:justify-center max-md:flex max-md:bottom-0">
-        {images?.map((_, index) => (
-          <li class="carousel-item">
-            <Slider.Dot index={index}>
-              <div class="py-2">
-                <div
-                  class="w-2.5 h-2.5 border border-solid border-[#e0e0e0] md:border-white bg-transparent group-disabled:bg-[#e0e0e0] md:group-disabled:bg-white rounded-[50%]"
-                  style={{ animationDuration: `${interval}s` }}
-                />
-              </div>
-            </Slider.Dot>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+                }}
+            />
+            <ul class="carousel justify-center col-span-full gap-6 z-8 row-start-4 max-md:absolute max-md:w-full max-md:justify-center max-md:flex max-md:bottom-0">
+                {images?.map((_, index) => (
+                    <li class="carousel-item">
+                        <Slider.Dot index={index}>
+                            <div class="py-2">
+                                <div
+                                    class="w-2.5 h-2.5 border border-solid border-[#e0e0e0] md:border-white bg-transparent group-disabled:bg-[#e0e0e0] md:group-disabled:bg-white rounded-[50%]"
+                                    style={{ animationDuration: `${interval}s` }}
+                                />
+                            </div>
+                        </Slider.Dot>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 function Buttons() {
-  return (
-    <>
-      <div class="flex items-center justify-center z-10 col-start-1 row-start-2">
-        <Slider.PrevButton class="btn btn-circle bg-transparent hover:bg-transparent border-0 shadow-none disabled:hidden">
-          <Icon
-            class="text-base-100"
-            size={24}
-            id="ChevronLeft"
-            strokeWidth={3}
-          />
-        </Slider.PrevButton>
-      </div>
-      <div class="flex items-center justify-center z-10 col-start-3 row-start-2">
-        <Slider.NextButton class="btn btn-circle bg-transparent hover:bg-transparent border-0 shadow-none disabled:hidden">
-          <Icon
-            class="text-base-100"
-            size={24}
-            id="ChevronRight"
-            strokeWidth={3}
-          />
-        </Slider.NextButton>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div class="flex items-center justify-center z-10 col-start-1 row-start-2">
+                <Slider.PrevButton class="btn btn-circle bg-transparent hover:bg-transparent border-0 shadow-none disabled:hidden">
+                    <Icon
+                        class="text-base-100"
+                        size={24}
+                        id="ChevronLeft"
+                        strokeWidth={3}
+                    />
+                </Slider.PrevButton>
+            </div>
+            <div class="flex items-center justify-center z-10 col-start-3 row-start-2">
+                <Slider.NextButton class="btn btn-circle bg-transparent hover:bg-transparent border-0 shadow-none disabled:hidden">
+                    <Icon
+                        class="text-base-100"
+                        size={24}
+                        id="ChevronRight"
+                        strokeWidth={3}
+                    />
+                </Slider.NextButton>
+            </div>
+        </>
+    );
 }
 
 function BannerCarousel(props: Props) {
-  const id = useId();
-  const { images, preload, interval } = { ...DEFAULT_PROPS, ...props };
+    const id = useId();
+    const { images, preload, interval } = { ...DEFAULT_PROPS, ...props };
 
-  return (
-    <div
-      id={id}
-      class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px] sm:min-h-min min-h-[660px]"
-    >
-      <Slider class="carousel carousel-center w-full col-span-full row-span-full gap-6">
-        {images?.map((image, index) => {
-          const params = { promotion_name: image.alt };
-          return (
-            <Slider.Item index={index} class="carousel-item w-full">
-              <BannerItem
-                image={image}
-                lcp={index === 0 && preload}
-                id={`${id}::${index}`}
-              />
-              <SendEventOnClick
-                id={`${id}::${index}`}
-                event={{ name: "select_promotion", params }}
-              />
-              <SendEventOnView
-                id={`${id}::${index}`}
-                event={{ name: "view_promotion", params }}
-              />
-            </Slider.Item>
-          );
-        })}
-      </Slider>
+    return (
+        <div
+            id={id}
+            class="grid grid-cols-[48px_1fr_48px] sm:grid-cols-[120px_1fr_120px] grid-rows-[1fr_48px_1fr_64px] sm:min-h-min min-h-[660px]"
+        >
+            <Slider class="carousel carousel-center w-full col-span-full row-span-full gap-6">
+                {images?.map((image, index) => {
+                    const params = { promotion_name: image.alt };
+                    return (
+                        <Slider.Item index={index} class="carousel-item w-full">
+                            <BannerItem
+                                image={image}
+                                lcp={index === 0 && preload}
+                                id={`${id}::${index}`}
+                            />
+                            <SendEventOnClick
+                                id={`${id}::${index}`}
+                                event={{ name: "select_promotion", params }}
+                            />
+                            <SendEventOnView
+                                id={`${id}::${index}`}
+                                event={{ name: "view_promotion", params }}
+                            />
+                        </Slider.Item>
+                    );
+                })}
+            </Slider>
 
-      {props.arrows && <Buttons />}
+            {props.arrows && <Buttons />}
 
-      {props.dots && <Dots images={images} interval={interval} />}
+            {props.dots && <Dots images={images} interval={interval} />}
 
-      <Slider.JS rootId={id} interval={interval && interval * 1e3} infinite />
-    </div>
-  );
+            <Slider.JS rootId={id} interval={interval && interval * 1e3} infinite />
+        </div>
+    );
 }
 
 export default BannerCarousel;
