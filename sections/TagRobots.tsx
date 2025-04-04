23cd9388157df31bf943835aac226eb76ac4205f
Claydson Fernando <claydson.silva@totvs.com.br>
2025-04-04 20:36:49 -0300

    fix: bugfix

--- sections/TagRobots.tsx
+++ sections/TagRobots.tsx
@@ -1,11 +1,16 @@
-// export { default } from "../islands/Robots.tsx";
-
 import { Head } from "$fresh/runtime.ts";
 
-export default function TagRobots() {
+export interface Props {
+  noindex?: string;
+  nofollow?: string;
+}
+
+export default function TagRobots({ noindex, nofollow }: Props) {
+  console.log({ noindex, nofollow });
+
   return (
     <Head>
-      <meta name="robots" content="noindex, follow" />
+      <meta name="robots" content={`${noindex || "index"}, ${nofollow || "follow"}`} />
     </Head>
   );
 }
