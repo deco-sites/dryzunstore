e0237ec6e6dc2969171c1b5fdf8046d3e8504e4e
Claydson Fernando <claydson.silva@totvs.com.br>
2025-04-04 20:05:14 -0300

    fix: bugfix

--- sections/ServerAnalytics.tsx
+++ sections/ServerAnalytics.tsx
@@ -2,9 +2,9 @@ function ServerAnalytics() {
   return (
     <>
         <script
-        dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://apiserver.dryzun.com.br/66dtdoewee.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','cr2fzz=aWQ9R1RNLVBLRDdDSk0%3D&apiKey=971fdf5a');`
-        }}
+          dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://apiserver.dryzun.com.br/66dtdoewee.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','cr2fzz=aWQ9R1RNLVBLRDdDSk0%3D&apiKey=971fdf5a');`
+          }}
         />
 
         
