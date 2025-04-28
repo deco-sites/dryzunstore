d45e167e1ec6c346176d76f7f47a14641d771d88
ERIC MACEDO GOMES <eric.gomes@totvs.com.br>
2025-04-28 17:58:43 -0300

    feat: ToqueDeAmor.tsx Desk

--- sections/ToqueDeAmor.tsx
+++ sections/ToqueDeAmor.tsx
@@ -24,63 +24,90 @@ export default function ToqueDeAmor(
 ) {
     return (
         <div class="container-2 py-8">
+            <style>
+                {`
+            @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+            `}
+            </style>
+
             {imagemCentral && (
-                <div class="flex justify-center">
-                    <img
-                        src={imagemCentral}
-                        class="max-w-full h-auto"
-                        alt="Imagem Central"
-                        loading="lazy"
-                    />
+                <div class="flex justify-center w-[1320px] mx-auto">
+                    <a href="https://www.dryzun.com.br/s?q=toque+de+amor">
+                        <img
+                            src={imagemCentral}
+                            class="max-w-full h-auto"
+                            alt="Imagem Central"
+                            loading="lazy"
+                        />
+                    </a>
                 </div>
             )}
 
             {textoMontserrat && (
-                <p class="text-center my-16 text-[#aa8a47] text-5xl">{textoMontserrat}</p>
+                <p class="text-center my-16 text-[#aa8a47] text-5xl font-bold tracking-widest" style={{ fontFamily: "Montserrat, sans-serif" }}>{textoMontserrat}</p>
             )}
 
             {textoDigital && (
-                <p class="text-2xl text-center text-[#545454] mb-16 mx-auto mt-0 w-[564px] h-[500px] bg-no-repeat bg-contain bg-center flex items-center bg-[url(https://dryzun.vtexassets.com/assets/vtex.file-manager-graphql/images/c3f1f913-a335-4fa5-b2b1-b5552edb0615___0914a307bb87b8e13f0c8efba8ee703e.svg)]">
+                <p style={{ fontFamily: "Montserrat, sans-serif" }} class="text-2xl text-center text-[#545454] mb-16 mx-auto mt-0 w-[564px] h-[500px] bg-no-repeat bg-contain bg-center flex items-center bg-[url(https://dryzun.vtexassets.com/assets/vtex.file-manager-graphql/images/c3f1f913-a335-4fa5-b2b1-b5552edb0615___0914a307bb87b8e13f0c8efba8ee703e.svg)]">
                     {textoDigital}
                 </p>
             )}
 
             {textoMeio && (
-                <div class="bg-[##f4f3f0] my-0 w-[1320px] mt-16 mx-auto">
-                    <p class="text-center text-[#545454] text-lg py-16 w-[640px]">{textoMeio}</p>
+                <div class="bg-[#f4f3f0] my-0 w-[1320px] mt-16 mx-auto">
+                    <p style={{ fontFamily: "Montserrat, sans-serif" }} class="text-center text-[#545454] text-lg py-8 w-[640px] mx-auto my-16 font-light">{textoMeio}</p>
                 </div>
             )}
 
-            {/* VIDEO */}
+            <div class="w-min my-16 mx-auto">
+                <iframe
+                    width="471"
+                    height="838"
+                    src="https://www.youtube.com/embed/xzOyH_wF6dk"
+                    title="Toque de Amor"
+                    frameborder="0"
+                    allow="accelerometer; 
+                    autoplay; 
+                    clipboard-write; 
+                    encrypted-media; 
+                    gyroscope; 
+                    picture-in-picture; 
+                    web-share"
+                    referrerpolicy="strict-origin-when-cross-origin"
+                    allowFullScreen>
+                </iframe>
+            </div>
 
-            <p class="text-[#39293d] my-0 mx-auto text-center text-lg">
+            <p style={{ fontFamily: "Montserrat, sans-serif" }} class="text-[#39293d] my-0 mx-auto text-center text-lg font-extralight">
                 Pingente  em ouro amarelo  Personalizado com a digital do seu filho
             </p>
 
-            <p class="text-[#39293d] my-6 mx-auto text-center text-lg">
+            <p style={{ fontFamily: "Montserrat, sans-serif" }} class="text-[#39293d] my-6 mx-auto text-center text-lg font-extralight">
                 (medindo 12mm sem corrente)
             </p>
 
-            <p class="text-[#39293d] my-0 mx-auto text-center text-lg">
+            <p style={{ fontFamily: "Montserrat, sans-serif" }} class="text-[#39293d] my-0 mx-auto text-center text-lg font-extralight">
                 Produzido sob encomenda, com prazo de 10 dias para entrega.
             </p>
 
             {textoMontserrat && (
-                <p class="text-center my-16 text-[#aa8a47] text-5xl">{textoMontserrat}</p>
+                <p style={{ fontFamily: "Montserrat, sans-serif" }} class="text-center my-16 text-[#aa8a47] text-5xl font-bold tracking-wide">{textoMontserrat}</p>
             )}
 
             {gridImagens && gridImagens.length > 0 && (
-                <div class="grid grid-cols-2 gap-0 mb-28 w-fit mx-auto">
-                    {gridImagens.slice(0, 4).map((imagem, index) => (
-                        <img
-                            key={index}
-                            src={imagem}
-                            class="h-auto"
-                            alt={`Imagem ${index + 1}`}
-                            loading="lazy"
-                        />
-                    ))}
-                </div>
+                <a href="https://www.dryzun.com.br/pingente-em-ouro-amarelo-toque-de-amor/p">
+                    <div class="grid grid-cols-2 gap-0 mb-28 w-fit mx-auto">
+                        {gridImagens.slice(0, 4).map((imagem, index) => (
+                            <img
+                                key={index}
+                                src={imagem}
+                                class="h-auto"
+                                alt={`Imagem ${index + 1}`}
+                                loading="lazy"
+                            />
+                        ))}
+                    </div>
+                </a>
             )}
         </div>
     );
