5d4c68443926e37dd5951da7136e1eee43442cfb
Claydson Fernando <claydson.silva@totvs.com.br>
2025-05-19 14:28:46 -0300

    fix: rollback gtm server

--- sections/ServerAnalytics.tsx
+++ sections/ServerAnalytics.tsx
@@ -4,17 +4,12 @@ function ServerAnalytics() {
       <script
         dangerouslySetInnerHTML={{
           __html:
-            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://apiserver.dryzun.com.br/66dtdoewee.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','cr2fzz=aWQ9R1RNLVBLRDdDSk0%3D&apiKey=971fdf5a');`,
+            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PKD7CJM');`,
         }}
       />
 
       <noscript>
-        <iframe
-          src="https://apiserver.dryzun.com.br/ns.html?id=GTM-PKD7CJM"
-          height="0"
-          width="0"
-          style="display:none;visibility:hidden"
-        >
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PKD7CJM" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
         </iframe>
       </noscript>
     </>
