511879f220425451ac7afe728d61ef954de21deb
Claydson Fernando <claydson.silva@totvs.com.br>
2025-06-03 23:02:34 -0300

    fix: bugfix

--- components/ui/CookieConsent.tsx
+++ components/ui/CookieConsent.tsx
@@ -63,7 +63,6 @@ const DEFAULT_PROPS = {
 
 function CookieConsent(props: Props) {
   const id = useId();
-  
   const { title, text, policy, buttons, layout } = {
     ...DEFAULT_PROPS,
     ...props,
