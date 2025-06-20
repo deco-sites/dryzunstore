c647df1f8dbd08be174e609b11c3ebfa7cf2372a
ERIC MACEDO GOMES <eric.gomes@totvs.com.br>
2025-06-20 17:23:01 -0300

    fix: buybuttonpdp props

--- components/product/AddToCartButton/common.tsx
+++ components/product/AddToCartButton/common.tsx
@@ -8,6 +8,9 @@ export interface Props {
     /** @description: sku name */
     eventParams: AddToCartParams;
     onAddItem: () => Promise<void>;
+    buttonColor?: string;
+    bgColor?: string;
+    buttonDisabled: any
 }
 
 const useAddToCart = ({ eventParams, onAddItem }: Props) => {
@@ -40,15 +43,18 @@ const useAddToCart = ({ eventParams, onAddItem }: Props) => {
 export default function AddToCartButton(props: Props) {
     const btnProps = useAddToCart(props);
 
-    console.log("dsadsa")
-    console.log("btnProps", props)
+    if (!props.buttonColor) props.buttonColor = "text-[#243959]"
+    if (!props.bgColor) props.bgColor = "bg-[#B4CBF0] hover:bg-[#81A1D4] hover:text-white"
 
     return (
         <Button
             {...btnProps}
-            class={`rounded-none btn shadow-none border-0 transition-[0.3s] flex h-12 justify-center items-center gap-2.5 self-stretch px-4 py-2.5 text-xs not-italic font-bold leading-[normal] tracking-[1.2px] uppercase ${props.buttonColor}  ${props.buttonDisabled
-                ? "bg-[#f5f5f5] cursor-not-allowed pointer-events-none"
-                : props.bgColor
+            class={`${props.buttonColor} rounded-none btn shadow-none border-0 transition-[0.3s] flex h-12 justify-center 
+                items-center gap-2.5 self-stretch px-4 py-2.5 text-xs not-italic font-bold 
+                leading-[normal] tracking-[1.2px] uppercase 
+                ${props.buttonDisabled
+                    ? "bg-[#f5f5f5] cursor-not-allowed pointer-events-none"
+                    : props.bgColor
                 }`}
         >
             {props.buttonDisabled
