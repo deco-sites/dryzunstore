8ea3ff8b71fe360e4ff30aa12016208ce955f8ca
ERIC MACEDO GOMES <eric.gomes@totvs.com.br>
2025-07-07 16:24:31 -0300

    feat: pagina '/welovesale-2025'

--- sections/BlackNovember/BlackNovember.tsx
+++ sections/BlackNovember/BlackNovember.tsx
@@ -1,54 +1,81 @@
 function BlackNovember() {
-  const items = [
-    {
-      src: "https://dryzun.vteximg.com.br/arquivos/mini banner(2).png",
-      link: "/summer-sale?tag=10-OFF",
-    },
-    {
-      src: "https://dryzun.vteximg.com.br/arquivos/mini-banner-25.png",
-      link: "/summer-sale?tag=25-OFF",
-    },
-    {
-      src: "https://dryzun.vteximg.com.br/arquivos/mini-banner7.png",
-      link: "/summer-sale?tag=30-OFF",
-    },
-    {
-      src: "https://dryzun.vteximg.com.br/arquivos/mini-banner8.png",
-      link: "/summer-sale?tag=40-OFF",
-    },
-    {
-      src: "https://dryzun.vteximg.com.br/arquivos/mini banner.png",
-      link: "/summer-sale?tag=relogios-15-OFF",
-    },
-    {
-      src: "https://dryzun.vteximg.com.br/arquivos/mini-relogio-banner-30.png",
-      link: "/summer-sale?tag=relogios-OFF",
-    },
-    // {
-    //     src: "https://dryzun.vteximg.com.br/arquivos/mini-banner11.png",
-    //     link: "/summer-sale?tag=todos",
-    // },
-  ];
+    const items = [
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/desk_30.png",
+            link: "/welovesale-2025?tag=30-OFF",
+            isMobile: false
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/desk_40.png",
+            link: "/welovesale-2025?tag=40-OFF",
+            isMobile: false
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/desk_50.png",
+            link: "/welovesale-2025?tag=50-OFF",
+            isMobile: false
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/desk_relogios.png",
+            link: "/welovesale-2025?tag=relogios-OFF",
+            isMobile: false
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/desk_todos.png",
+            link: "/welovesale-2025",
+            isMobile: false
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/mob_30.png",
+            link: "/welovesale-2025?tag=30-OFF",
+            isMobile: true
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/mob_40.png",
+            link: "/welovesale-2025?tag=40-OFF",
+            isMobile: true
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/mob_50.png",
+            link: "/welovesale-2025?tag=50-OFF",
+            isMobile: true
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/mob_relogios.png",
+            link: "/welovesale-2025?tag=relogios-OFF",
+            isMobile: true
+        },
+        {
+            src: "https://dryzun.vteximg.com.br/arquivos/mob_produtos.png",
+            link: "/welovesale-2025",
+            allMobile: true,
+            isMobile: true
+        }
+    ];
 
-  return (
-    <>
-      <div className="flex flex-wrap justify-center gap-4 w-full">
-        {items.map((item, index) => (
-          <a key={index} href={item.link}>
-            <img
-              loading="lazy"
-              src={item.src}
-              width={132}
-              height={132}
-              className="object-contain"
-            />
-          </a>
-        ))}
-      </div>
+    return (
+        <>
+            <div className="flex flex-wrap justify-center gap-4 w-full">
+                {items.map((item, index) => (
+                    <a
+                        key={index}
+                        href={item.link}
+                        className={item.isMobile ? "block md:hidden" : "hidden md:block"}
+                    >
+                        <img
+                            loading="lazy"
+                            src={item.src}
+                            width={item.allMobile ? 280 : 132}
+                            height={132}
+                            className="object-contain"
+                        />
+                    </a>
+                ))}
+            </div>
 
-      <h1 className="container-2 mt-12 uppercase">Summer Sale</h1>
-    </>
-  );
+            <h1 className="container-2 mt-12 uppercase">We Love Sale 2025</h1>
+        </>
+    );
 }
 
 export default BlackNovember;
